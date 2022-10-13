41c2d2707cd59d274d97d8950b4f399d6be09dcf
unknown <mariyachunawala7@gmail.com>
2022-10-13 14:01:19 +0530

    Change poster config

--- src/config/Poster.config.js
+++ src/config/Poster.config.js
@@ -1,33 +1,34 @@
 const settings = {
     infinite: false,
-    autoplay: false,
+    speed: 500,
     slidesToShow: 5,
-    slidesToScroll: 2,
+    slidesToScroll: 4,
     initialSlide: 0,
     responsive: [
         {
-            breakpoints: 1024,
+            breakpoint: 1024,
             settings: {
                 slidesToShow: 3,
-                slidesToScroll: 2,
-                infinite: true
-            }
+                slidesToScroll: 3,
+                infinite: true,
+            },
         },
         {
-            breakpoints: 600,
+            breakpoint: 600,
             settings: {
                 slidesToShow: 2,
-                slidesToScroll: 1,
-                initialScale: 1
-            }
+                slidesToScroll: 2,
+                initialSlide: 2,
+            },
         },
         {
-            breakpoints: 480,
+            breakpoint: 480,
             settings: {
                 slidesToShow: 3,
                 slidesToScroll: 1,
-            }
-        }
-    ]
-}
+            },
+        },
+    ],
+};
+
 export default settings;
