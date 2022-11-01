8c568b213ad15d9624c9bb5c5561d2c3a2e16f47
unknown <mariyachunawala7@gmail.com>
2022-11-01 17:12:45 +0530

    Connect TMDB API with frontend

--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -2,19 +2,41 @@ import React, { useEffect, useState } from "react";
 import EntertainmentCardSlider from "../components/Entertainment/EntertainmentCard.component";
 import PosterSlider from "../components/PosterSlider/PosterSlider.component";
 
-// config
-import TempImages from "../../src/config/TempImages.config"
 import axios from "axios";
 
 const HomePage = () => {
     const [popularMovies, setPopularMovies] = useState([]);
+    const [topRatedMovies, setTopRatedMovies] = useState([]);
+    const [upcomingMovies, setUpcomingMovies] = useState([]);
+    const [nowPlayingMovies, setNowPlayingMovies] = useState([]);
     useEffect(() => {
         const requestPopularMovies = async () => {
             const getPopularMovies = await axios.get('/movie/popular');
             setPopularMovies(getPopularMovies.data.results);
         }
         requestPopularMovies();
-    }, [])
+    }, []);
+    useEffect(() => {
+        const requestTopRatedMovies = async () => {
+            const getTopRatedMovies = await axios.get('/movie/top_rated');
+            setTopRatedMovies(getTopRatedMovies.data.results);
+        }
+        requestTopRatedMovies();
+    }, []);
+    useEffect(() => {
+        const requestUpcomingMovies = async () => {
+            const getUpcomingMovies = await axios.get('/movie/upcoming');
+            setUpcomingMovies(getUpcomingMovies.data.results);
+        }
+        requestUpcomingMovies();
+    }, []);
+    useEffect(() => {
+        const requestNowPlayingMovies = async () => {
+            const getNowPlayingMovies = await axios.get('/movie/upcoming');
+            setNowPlayingMovies(getNowPlayingMovies.data.results);
+        }
+        requestNowPlayingMovies();
+    }, []);
     return <>
         <div className="flex flex-col gap-16">
             <div className="container mx-auto px-auto">
@@ -35,16 +57,16 @@ const HomePage = () => {
                 </div>
             </div>
             <div className="container mx-auto px-4">
-                <PosterSlider images={TempImages} title="Online Streaming Events" isDark={false} />
+                <PosterSlider images={topRatedMovies} title="Online Streaming Events" isDark={false} />
             </div>
             <div className="container mx-auto px-4">
-                <PosterSlider images={TempImages} title="Outdoor Events" isDark={false} />
+                <PosterSlider images={upcomingMovies} title="Outdoor Events" isDark={false} />
             </div>
             <div className="container mx-auto px-4">
-                <PosterSlider images={TempImages} title="Laughter Therapy" isDark={false} />
+                <PosterSlider images={nowPlayingMovies} title="Laughter Therapy" isDark={false} />
             </div>
             <div className="container mx-auto px-4">
-                <PosterSlider images={TempImages} title="Popular Events" isDark={false} />
+                <PosterSlider images={topRatedMovies} title="Popular Events" isDark={false} />
             </div>
         </div>
     </>
